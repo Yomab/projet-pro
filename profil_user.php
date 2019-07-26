38d0aaa58caa25cd376dad3bf2d66c28d3b7712e
Yomab <mabire.yoan@gmail.com>
2019-07-26 16:16:49 +0200

    form terminé

--- profil_user.php
+++ profil_user.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+          <link rel="stylesheet" href="assets/materialize.css">
         <link rel="stylesheet" href="style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
         <title>Profil_user</title>
@@ -78,11 +78,11 @@
          <label for="confirm_password" class="center-align brown-text text-darken-4">Confirmation :</label>
        </div>
      </div>
-     <div class="row ">
-       <div class="input-field center col s12">
+     <div class="row buttonSet">
+       <div class="input-field center col s6 l6">
          <input type="submit" class="btn waves-effect waves-light col s6 formInscription" value="valider modification" />
        </div>
-       <div class="input-field center col s12">
+       <div class="input-field center col s6 l6">
          <input type="submit" class="btn waves-effect waves-light col s6 formInscription" value="supprimer le compte" />
        </div>
      </div>
@@ -96,6 +96,7 @@
       <script src = "https://code.jquery.com/jquery-3.4.0.js" integrity = "sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin = "anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <script src="assets/materialize.js"></script>
       <script src="main.js"></script>
       </body>
       </html>
